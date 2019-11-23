206f6ff66066a291618a90d69ef9a12e9e682fc1
Jacob David C. Cunningham <jcun4128@gmail.com>
2019-11-23 13:13:55 -0600

    basic stuff, hardly any styling, need font but later, need to get core functionality done

--- front-end/app/src/pages/qr-scanner/QRScanner.js
+++ front-end/app/src/pages/qr-scanner/QRScanner.js
@@ -1,21 +1,48 @@
 import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import './QRScanner.scss';
 import QrcodeDecoder from 'qrcode-decoder';
 
-function QRScanner() {
+function QRScanner(props) {
     const qr = new QrcodeDecoder();
 
     const qrCallback = (videoElement) => {
-        qr.decodeFromVideo(videoElement).then((res) => {
-            console.log(res); // has data
+        qr.decodeFromVideo(videoElement)
+        .then((res) => {
+            if (res.data && res.data.length) {
+                const qrNotePath = "/user/" + res.data;
+                qr.stop();
+                // return (<Redirect to={qrNotePath} />); // doesn't redirect
+                props.history.push(qrNotePath, {some: 'state'});
+            } else {
+                alert('Failed to scan QR code');
+            }
         })
         .catch((err) => {
-           console.log(err); 
+            alert('Failed to scan QR code'); // I don't know when this happens/haven't been able to trigger it
         });
     };
 
-    const QRScannerInput = <video id="QRNotepad__video-playback" />;
+    // this needs file upload to work
+    const decodeFromImage = (img) => {
+        qr.decodeFromImage(img)
+            .then((res) => {
+
+            })
+            .catch((err) => {
+                alert("Failed to decode QR from file");
+            });
+    };
+
+    // yikes
+    const QRScannerInput = <>
+        <h2 className="QRNotepad__qr-scanner-title">Scan QR Code</h2>
+        <video id="QRNotepad__video-playback" />
+        {// this needs file upload to work
+        /* <h3 className="QRNotepad__qr-scanner-or">- OR -</h3>
+        <label htmlFor="QRNotepad__file-upload">Upload File</label>
+        <input id="QRNotepad__file-upload" type="file" /> */}
+    </>;
 
     const videoStream = () => {
         const constraints = { audio: false, video: 480, height: 320 };
@@ -28,8 +55,10 @@ function QRScanner() {
                 video.onloadedmetadata = (e) => {
                     // video.onplay();
                     console.log(e);
-                    qrCallback(video);
                 }
+                video.onloadeddata = () => {
+                    qrCallback(video);
+                };
                 video.play();
             })
             .catch((err) => {
